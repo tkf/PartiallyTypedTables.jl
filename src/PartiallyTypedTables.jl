b0b2cca3f6c6ed03c609251fc80e7dfef97d9447
Takafumi Arakaki <aka.tkf@gmail.com>
2020-02-07 22:46:55 -0800

    Files generated by PkgTemplates

---
+++ src/PartiallyTypedTables.jl
@@ -0,0 +1,5 @@
+module PartiallyTypedTables
+
+greet() = print("Hello World!")
+
+end # module
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using PartiallyTypedTables
+using Test
+
+@testset "PartiallyTypedTables.jl" begin
+    # Write your own tests here.
+end
